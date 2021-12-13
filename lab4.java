524b1f8f9e873709405429596ea7f1a8756c2238
Asphodell <91069241+Asphodell@users.noreply.github.com>
2021-12-13 20:22:16 +0300

    added explanations for the user

--- lab4.java
+++ lab4.java
@@ -5,18 +5,20 @@ public class lab4 {
     public static void main(String[] arg) {
         try {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Введите количество элементов в массиве:");
             int n = sc.nextInt();
             double[] array = new double[n];
             for (int i = 0; i < n; i++) {
+                System.out.println("Введите " + (i + 1)+ "-й" + " элемент массива");
                 array[i] = sc.nextDouble();
             }
-            System.out.println(Arrays.toString(array));
+            System.out.println("Оригинальное состояние массива: " + Arrays.toString(array));
             double[] resultArray = new double[n];
             for (int j = 1; j <= n; j++) {
                     double result = avgArifm(Arrays.copyOf(array, j));
                     resultArray[j - 1] = result;
                 }
-            System.out.println(Arrays.toString(resultArray));
+            System.out.println("Итоговое состояние массива: " + Arrays.toString(resultArray));
         }
         catch (InputMismatchException e) {
             System.out.println("Error");
